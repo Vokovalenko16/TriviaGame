603645e28e0ff4f1dce45679026c2da9e4873f52
Volodymyr Kovalenko <vokovalenko16@gmail.com>
2019-01-17 00:08:09 -0800

    csshtmldone

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+const ask1 = "Which warrior holds the record for most 3 pointers made in one game?"
+
+
+
+
+const ask2 = "Which Golden State warriors player is always on bad behavior?"
+const ask3 = "Which warrior is possibly the best shooter of All Time?"
+const ask4 = "Which team beat Golden State in 2016 Finals?"
+
+
+
+
+Timer = 20;
+
+
