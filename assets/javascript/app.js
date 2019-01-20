37b5f53b50489991cd3c419e3e9af5b975f788c8
Volodymyr Kovalenko <vokovalenko16@gmail.com>
2019-01-19 19:26:08 -0800

    done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,92 @@
 $(document).ready(function() {
 
-const ask1 = "Which warrior holds the record for most 3 pointers made in one game?"
-const ask2 = "Which Golden State warriors player is always on bad behavior?"
-const ask3 = "Which warrior is possibly the best shooter of All Time?"
-const ask4 = "Which team beat Golden State in 2016 Finals?"
+const questionArr1  = ["Which warrior holds the record for most 3 pointers made in one game?",
+"Which Golden State warriors player is always on bad behavior?",
+"Which warrior is possibly the best shooter of All Time?",
+"Who is Golden States most hated player?"]
 
-const answerArr1 = ["Klay Thompson", "Steph Curry", "Kevin Drant", "Draymond Green"]
-const answerArr2 = ["Boston Celtics", "Chicago Bulls", "Cleveland Cavs", "Los Angles Lakers"]
+const answerArr1 = ["Klay Thompson", "Draymond Green","Steph Curry", "Kevin Durant",]
 
 let startB = $(".startbutton1")
 
-Timer = 20;
+Timer = 5;
 Active = false;
+Score = 0;
+
+
 
 $(".startbutton1").on("click", letsGo);
+
+// function clickedAnswer() {
+// console.log(this);
+// }
 
 function letsGo(callback) {
     if (!Active) {
     Active = true;
-    setInterval(Ticking, 1000);
+    let Timeout1 = setTimeout(randAnswer, 0);
+    let Timeout2 = setTimeout(randAnswerA, 5000);
+    let Timeout3 = setTimeout(randAnswerB, 10000);
+    let Timeout4 = setTimeout(randAnswerC, 15000);
+    let Timeout5 = setTimeout(Ending, 20000);
+    let myLet = setInterval(Ticking, 1000);
+    return;
 }};
-
-
 function Ticking() {
-    if (Active = true && Timer !== 0) {
+    if (Active = true) {
     Timer--
+    if(Timer === 0) {
+        Timer = 5; 
+    }
     $(".clock").html(Timer)
-    $(".question").html(ask1)
     }}
 
-function stop() {
-    if (Timer===0){
-    Active = False;
-    }
+   function regenerateAnswers(answers) {
+for(var i=0; i<answerArr1.length; i++){
+    let pickS = ".answer" + (i+1);
+    $(pickS).html(answers[i]);
+    $(pickS).on("click",yourpickS);
+}};
+
+function yourpickS() {
+    console.log(this)
 }
 
 function randAnswer() {
-    for (var i=0; i<answerArr1.length; i++){
-        console.log(answerArr1[i]);
+    if (Active = true) {
+        $(".question").html(questionArr1[0]);
+        regenerateAnswers([answerArr1[0], answerArr1[1], answerArr1[2], answerArr1[3]]);
     }
 }
 
-function reset() {
-    $(".question").html("");
-    $(".answer1").html("");
-}
-    
-function questionA() {
-    if (Active = true){
-        $(".question").html(ask1)
-    }
-}
+    function randAnswerA() {
+        if (Active = true) {
+            $(".question").html(questionArr1[1]);
+            regenerateAnswers([answerArr1[3], answerArr1[2], answerArr1[1], answerArr1[0]]);
+        }}
+
+        function randAnswerB() {
+            if (Active = true) {
+                $(".question").html(questionArr1[2]);
+                regenerateAnswers([answerArr1[0], answerArr1[3], answerArr1[2], answerArr1[1]]);
+
+            }}
+            function randAnswerC() {
+                if (Active = true) {
+                    $(".question").html(questionArr1[3]);
+                    regenerateAnswers([answerArr1[3], answerArr1[1], answerArr1[2], answerArr1[0]]);
+                }}
+                function Ending() {
+                    $(".startbutton1").hide();
+                    $(".timemessage").hide();
+                    $(".clock").hide();
+                     $(".answer1").hide();
+                     $(".answer2").hide();
+                     $(".answer3").hide();
+                     $(".answer4").hide();
+                     $(".question").hide();
+                     $(".answers").html("<p> 1. Klay Thompson <p><br><p> 2. Draymound Green <p><br><p> 3. Steph Curry <p><br><p> 4. Kevin Durant <p><br>")
+                    }
 
 });
 
