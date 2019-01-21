b82a663576b64d45abdebc8e13c61643264bf678
Volodymyr Kovalenko <vokovalenko16@gmail.com>
2019-01-20 17:02:57 -0800

    Finished some Unorthadox code, but works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,34 +9,35 @@ const answerArr1 = ["Klay Thompson", "Draymond Green","Steph Curry", "Kevin Dura
 
 let startB = $(".startbutton1")
 
-Timer = 5;
+Timer = 10;
 Active = false;
 Score = 0;
-
+let Answers = [];
+let tempvalA = [];
+let tempvalB = [];
+let tempvalC = [];
+let tempvalD = [];
+let tempvalE = [];
 
 
 $(".startbutton1").on("click", letsGo);
 
-// function clickedAnswer() {
-// console.log(this);
-// }
-
 function letsGo(callback) {
     if (!Active) {
     Active = true;
-    let Timeout1 = setTimeout(randAnswer, 0);
-    let Timeout2 = setTimeout(randAnswerA, 5000);
-    let Timeout3 = setTimeout(randAnswerB, 10000);
-    let Timeout4 = setTimeout(randAnswerC, 15000);
-    let Timeout5 = setTimeout(Ending, 20000);
-    let myLet = setInterval(Ticking, 1000);
+    var Timeout1 = setTimeout(randAnswer, 0);
+    var Timeout2 = setTimeout(randAnswerA, 10000);
+    var Timeout3 = setTimeout(randAnswerB, 20000);
+    var Timeout4 = setTimeout(randAnswerC, 30000);
+    var Timeout5 = setTimeout(Ending, 40000);
+    var myLet = setInterval(Ticking, 1000);
     return;
 }};
 function Ticking() {
     if (Active = true) {
     Timer--
     if(Timer === 0) {
-        Timer = 5; 
+        Timer = 10; 
     }
     $(".clock").html(Timer)
     }}
@@ -45,12 +46,53 @@ function Ticking() {
 for(var i=0; i<answerArr1.length; i++){
     let pickS = ".answer" + (i+1);
     $(pickS).html(answers[i]);
+    $(pickS).off();
     $(pickS).on("click",yourpickS);
 }};
 
 function yourpickS() {
-    console.log(this)
+    let v = $(this).html()
+    Answers.push(v)
+    tempvalA.push(v)
+    if (tempvalA.length === 1) {
+        tempvalB.push(v)
+        if (tempvalB == answerArr1[0]) {
+            Score++
+            $("#sc1").html(Score)
+            $("#sc2").html(Score)
+            tempvalB.pop();
+        }
+    }
+    else if (tempvalA.length === 2){
+            tempvalC.push(v)
+            if (tempvalC == answerArr1[1]) {
+                Score++
+                $("#sc1").html(Score)
+                $("#sc2").html(Score)
+                tempvalC.pop();
+    }}
+    else if (tempvalA.length ===3){
+        tempvalD.push(v)
+        if (tempvalD == answerArr1[2]) {
+            Score++
+            $("#sc1").html(Score)
+            $("#sc2").html(Score)
+            tempvalD.pop();
+    }}
+    else if (tempvalA.length ===4){
+        tempvalE.push(v)
+        if (tempvalE == answerArr1[3]) {
+            Score++
+            $("#sc1").html(Score)
+            $("#sc2").html(Score)
+    }}
+    console.log(tempvalA);
+    console.log(tempvalB);
+    console.log(tempvalC);
+    console.log(tempvalD);
+    console.log(tempvalE);
 }
+            
 
 function randAnswer() {
     if (Active = true) {
@@ -77,6 +119,8 @@ function randAnswer() {
                     regenerateAnswers([answerArr1[3], answerArr1[1], answerArr1[2], answerArr1[0]]);
                 }}
                 function Ending() {
+                    $("#sc1").hide();
+                    $("#sc2").hide();
                     $(".startbutton1").hide();
                     $(".timemessage").hide();
                     $(".clock").hide();
@@ -85,9 +129,10 @@ function randAnswer() {
                      $(".answer3").hide();
                      $(".answer4").hide();
                      $(".question").hide();
-                     $(".answers").html("<p> 1. Klay Thompson <p><br><p> 2. Draymound Green <p><br><p> 3. Steph Curry <p><br><p> 4. Kevin Durant <p><br>")
+                     $(".answers").html( "<p> Correct Answers: </p><p>1. Klay Thompson </p><p> 2. Draymound Green </p><p> 3. Steph Curry </p><p> 4. Kevin Durant </p> <br> <span> <h1> You Got: " + Score + " Right! </h1></span>")
                     }
-
+            
+                       
 });
 
 
